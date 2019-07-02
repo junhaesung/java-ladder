efc5915ffdc067a7723b9bc2313bd3cada99c7ea
Haeseong Jeon <seong0428@gmail.com>
2019-07-03 02:40:04 +0900

    test #step2 - 테스트케이스를 추가합니다

---
+++ src/test/java/nextstep/ladder/LadderApplicationTest.java
@@ -0,0 +1,11 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.Test;
+
+public class LadderApplicationTest {
+    @Test
+    void name() {
+        LadderApplication ladderApplication = new LadderApplication();
+        ladderApplication.run();
+    }
+}
