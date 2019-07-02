09688d479214931529458ee2f1fe015faa1aed2b
Haeseong Jeon <seong0428@gmail.com>
2019-07-03 03:18:13 +0900

    feat #step2 - Ladder 클래스를 추가합니다

--- src/main/java/nextstep/ladder/LadderApplication.java
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -1,14 +1,12 @@
 package nextstep.ladder;
 
-import nextstep.ladder.domain.Rung;
+import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.view.ConsoleInputView;
 import nextstep.ladder.view.ConsoleResultView;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class LadderApplication {
     private final InputView inputView;
@@ -33,52 +31,13 @@ public class LadderApplication {
 
         int height = inputView.inputHeight();
 
+        Ladder ladder = new Ladder();
         // init
-        List<List<Rung>> ladder = new ArrayList<>();
-        for (int i = 0; i < names.size(); i++) {
-            List<Rung> line = new ArrayList<>();
-            for (int j = 0; j < height; j++) {
-                line.add(Rung.NONE);
-            }
-            ladder.add(line);
-        }
-
+        ladder.initialize(names, height);
         // draw line
-        for (int i = 0; i < ladder.size() - 1; i++) {
-            for (int j = 0; j < height; j++) {
-                Random random = new Random();
-                if (random.nextInt(2) == 0) {
-                    continue;
-                }
-                List<Rung> line1 = ladder.get(i);
-                if (line1.get(j) != Rung.NONE) {
-                    continue;
-                }
-                List<Rung> line2 = ladder.get(i + 1);
-                if (line2.get(j) != Rung.NONE) {
-                    continue;
-                }
-                line1.remove(j);
-                line1.add(j, Rung.RIGHT);
-                line2.remove(j);
-                line2.add(j, Rung.LEFT);
-            }
-        }
+        ladder.drawLine();
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < height; i++) {
-            stringBuilder.append("    ");
-            for (int j = 0; j < ladder.size(); j++) {
-                stringBuilder.append("|");
-                if (ladder.get(j).get(i) == Rung.RIGHT) {
-                    stringBuilder.append("-----");
-                } else {
-                    stringBuilder.append("     ");
-                }
-            }
-            stringBuilder.append("\n");
-        }
-        String formattedLadder = stringBuilder.toString();
+        String formattedLadder = ladder.format(height);
 
         resultView.print(names, formattedLadder);
     }
---
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -0,0 +1,60 @@
+package nextstep.ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Ladder {
+    private List<List<Rung>> ladder;
+
+    public void initialize(List<String> names, int height) {
+        ladder = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++) {
+            List<Rung> line = new ArrayList<>();
+            for (int j = 0; j < height; j++) {
+                line.add(Rung.NONE);
+            }
+            ladder.add(line);
+        }
+    }
+
+    public void drawLine() {
+        for (int i = 0; i < ladder.size() - 1; i++) {
+            for (int j = 0; j < ladder.get(i).size(); j++) {
+                Random random = new Random();
+                if (random.nextInt(2) == 0) {
+                    continue;
+                }
+                List<Rung> line1 = ladder.get(i);
+                if (line1.get(j) != Rung.NONE) {
+                    continue;
+                }
+                List<Rung> line2 = ladder.get(i + 1);
+                if (line2.get(j) != Rung.NONE) {
+                    continue;
+                }
+                line1.remove(j);
+                line1.add(j, Rung.RIGHT);
+                line2.remove(j);
+                line2.add(j, Rung.LEFT);
+            }
+        }
+    }
+
+    public String format(int height) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            stringBuilder.append("    ");
+            for (int j = 0; j < ladder.size(); j++) {
+                stringBuilder.append("|");
+                if (ladder.get(j).get(i) == Rung.RIGHT) {
+                    stringBuilder.append("-----");
+                } else {
+                    stringBuilder.append("     ");
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
